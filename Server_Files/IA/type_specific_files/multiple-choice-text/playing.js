89144264eee43bd13273c2e53c3ddb79b5e2256f
3aceskelsen <zac.eskelsen@gmai.com>
2013-04-04 10:23:10 -0600

    made minor changes - getting familiar with project

--- Server_Files/IA/type_specific_files/multiple-choice-text/playing.js
+++ Server_Files/IA/type_specific_files/multiple-choice-text/playing.js
@@ -2,8 +2,7 @@ var thigie = function() {
 	var response = $("<span></span>");
 	response.attr("id", "response");
 	response.text("this is just a test");
-	$("#radioButton1").parent().append(response);
-	
-	
-	
+	//IsLog.c(this);
+	response.css("color", "purple").css("display","block").css("padding-left","2em");
+	$(this).parent().append(response);
 }
