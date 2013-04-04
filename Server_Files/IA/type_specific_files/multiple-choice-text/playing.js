d33d0c5d1c470846d28890b345bc566d82195801
3aceskelsen <zac.eskelsen@gmai.com>
2013-04-04 08:28:56 -0600

    changed to files

---
+++ Server_Files/IA/type_specific_files/multiple-choice-text/playing.js
@@ -0,0 +1,9 @@
+var thigie = function() {
+	var response = $("<span></span>");
+	response.attr("id", "response");
+	response.text("this is just a test");
+	$("#radioButton1").parent().append(response);
+	
+	
+	
+}
