5d192efc070d2bbfe657341f7849ec8b031f462c
3aceskelsen <zac.eskelsen@gmai.com>
2013-05-01 10:51:58 -0600

    Completed the multiple choice interaction

--- Server_Files/IA/type_specific_files/multi-choice/repeater.js
+++ Server_Files/IA/type_specific_files/multi-choice/repeater.js
@@ -39,5 +39,17 @@ function() {
 				assessmentElements[i].setEvents();
 			}
 		}
-	}	
+	}
+	//	This is for the wysiwyg
+	//	Add in the wysiwyg if requested.
+	/*IsLog.c("ATTENTION: We are calling the wysiwyg.js file");
+	tinymce.init({
+			selector: "textarea",
+			plugins: [
+			"advlist autolink lists link image charmap print preview anchor",
+			"searchreplace visualblocks code fullscreen",
+			"insertdatetime media table contextmenu paste "
+		],
+		toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image"
+	});*/
 }
